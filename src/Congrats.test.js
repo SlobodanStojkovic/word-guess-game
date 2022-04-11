21fe84e05ee5f3af490d1c32041834ceb6e5645c
Slobodan Stojkovic <sc.slobodanstojkovic@gmail.com>
2022-04-11 12:14:46 +0200

    Add start tests for GuessedWords

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -1,10 +1,6 @@
-import Enzyme, { shallow, ShallowWrapper } from "enzyme";
-import EnzymeAdapter from "@wojtekmaj/enzyme-adapter-react-17";
-
+import { shallow, ShallowWrapper } from "enzyme";
 import Congrats from "./Congrats";
-import { findByTestAttr } from "../test/testUtils";
-
-Enzyme.configure({ adapter: new EnzymeAdapter() });
+import { findByTestAttr, checkProps } from "../test/testUtils";
 
 const defaultProps = { success: false };
 
@@ -20,13 +16,11 @@ const setup = (props = {}) => {
   return shallow(<Congrats {...setupProps} />); //example if props = { success: true } then {...props} will be success=true
 };
 
-
 test("renders without error", () => {
   const wrapper = setup({ success: false });
   const component = findByTestAttr(wrapper, "component-congrats");
   expect(component.length).toBe(1);
 });
-
 
 test("renders no text when success prop is false", () => {
   const wrapper = setup({ success: false });
@@ -34,20 +28,11 @@ test("renders no text when success prop is false", () => {
   expect(component.text()).toBe("");
 });
 
-
 test("renders non empty congrats message when success prop is true", () => {
   const wrapper = setup({ success: true });
   const message = findByTestAttr(wrapper, "congrats-message");
   expect(message.text().length).not.toBe(0);
 });
-
-
-test("renders non empty congrats message when success prop is false", () => {
-  const wrapper = setup({ success: true });
-  const message = findByTestAttr(wrapper, "congrats-message");
-  expect(message.text().length).not.toBe(0);
-});
-
 
 test("does not throw warning with expected props", () => {
   const expectedProps = { success: "no success" };
